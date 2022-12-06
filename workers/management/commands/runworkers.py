63debb7278474ca8beecd5ce9211b0ea16a2af3d
Pol Feliu Cuberes <>
2022-12-06 09:38:06 +0100

    implemented workers concurrency with std ThreadPoolExecutor

--- workers/management/commands/runworkers.py
+++ workers/management/commands/runworkers.py
@@ -1,7 +1,10 @@
 import json
 import logging
+import queue
 import signal
 import time
+from multiprocessing import Queue
+from concurrent.futures import ThreadPoolExecutor
 
 from django.core.management.base import BaseCommand
 from django.utils import timezone
@@ -9,10 +12,12 @@ from django.utils import timezone
 from ...models import Task
 from ...util import autodiscover
 from ...worker import registry, scheduled
-from ...settings import SLEEP, PURGE
-
+from ...settings import SLEEP, PURGE, WORKERS_CONCURRENCY
 
 log = logging.getLogger(__name__)
+
+work_queue = Queue()
+completed_queue = Queue()
 
 
 class Command(BaseCommand):
@@ -34,29 +39,30 @@ class Command(BaseCommand):
         for t in scheduled:
             Task.create_scheduled_task(t['handler'], t['schedule'])
 
+        worker_pool = ThreadPoolExecutor(WORKERS_CONCURRENCY)
+        worker_pool.map(worker, range(WORKERS_CONCURRENCY))
+        to_be_completed_tasks = []
+
         log.debug('worker: ready for tasks...')
         while not self.__SIGINT:
+
+            while True:
+                try:
+                    id = completed_queue.get(block=False)
+                    to_be_completed_tasks.remove(id)
+                except queue.Empty:
+                    break
+
             tasks = Task.objects.filter(run_at__lte=timezone.now(), completed_at=None)
 
             if tasks:
                 for task in tasks:
-                    log.debug('worker: running {0}'.format(task.handler))
-                    args = json.loads(task.args)
-                    kwargs = json.loads(task.kwargs)
+                    if task.id in to_be_completed_tasks:
+                        # Already queued
+                        continue
 
-                    try:
-                        registry[task.handler](*args, **kwargs)
-                        task.status = Task.COMPLETED
-                    except Exception as e:
-                        task.status = Task.FAILED
-                        task.error = str(e)
-                        log.exception(e)
-
-                    task.completed_at = timezone.now()
-                    task.save()
-
-                    if task.schedule:
-                        Task.create_scheduled_task(task.handler, task.schedule)
+                    to_be_completed_tasks.append(task.id)
+                    work_queue.put(task.id)
 
                 # if PURGE is 1000, we will keep the latest 1000 completed tasks
                 keep = (
@@ -72,3 +78,28 @@ class Command(BaseCommand):
                     Task.objects.exclude(pk__in=keep).filter(status=Task.COMPLETED).delete()
             else:
                 time.sleep(SLEEP)
+
+
+def worker(worker_id):
+    while True:
+        id = work_queue.get()
+        task = Task.objects.get(id=id)
+
+        log.debug('worker: running {0}'.format(task.handler))
+        args = json.loads(task.args)
+        kwargs = json.loads(task.kwargs)
+
+        try:
+            registry[task.handler](*args, **kwargs)
+            completed_queue.put(id)
+            task.status = Task.COMPLETED
+        except Exception as e:
+            task.status = Task.FAILED
+            task.error = str(e)
+            log.exception(e)
+
+        task.completed_at = timezone.now()
+        task.save()
+
+        if task.schedule:
+            Task.create_scheduled_task(task.handler, task.schedule)
